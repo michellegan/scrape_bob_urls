2d26220f6e31711ca1d740c8be3aa4f9ad0803d1
michellegan <53538332+michellegan@users.noreply.github.com>
2021-03-18 16:59:42 -0700

    Add files via upload

---
+++ get_bob.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import requests
+import json
+import time
+
+API_KEY = 'AIzaSyD-qE7PtKfqSxo9Q85Frek7UkOSPFoS7mg'
+RADIUS = 10000
+CITIES = {'San Francisco': '37.7749,-122.4194'}
+
+def parse_result (city, results):
+    print (result['place_id'])
+    arr = [city, result['name'], result['place_id'], result['types'], result['rating'], result['user_ratings_total']]
+    return arr
+
+places_data = pd.DataFrame(columns=['city','name', 'place_id', 'types', 'avg_rating', 'num_ratings'])
+
+for city, coord in CITIES.items():
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + coord + '&type=restaurant'+ '&radius=' + str(RADIUS) + '&key=' + API_KEY
+    response = requests.get(url).json()
+    results = response['results']
+    for result in results:
+        if 'rating' not in result:
+            continue
+        result_arr = parse_result (city, results)
+        places_data.loc[len(places_data)] = result_arr
+    while 'next_page_token' in response: 
+        URL = url+  '&pagetoken=' + response['next_page_token']
+        time.sleep(5)
+        response = requests.get(URL).json()
+        for result in response['results']:
+            if 'rating' not in result:
+                continue
+            result_arr = parse_result(city, results)
+            places_data.loc[len(places_data)] = result_arr
+    
+
+print(places_data)
